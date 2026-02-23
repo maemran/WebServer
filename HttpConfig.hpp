3fb3f36dfcb9bd661000dd57942b5ee8473745d2
maemran <maemran@student.42amman.com>
2026-02-23 11:52:02 +0300

    add congig file headers

---
+++ HttpConfig.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HttpConfig.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maemran <maemran@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 11:44:40 by maemran           #+#    #+#             */
+/*   Updated: 2026/02/23 11:50:58 by maemran          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HTTPCONFIG_HPP
+#define HTTPCONFIG_HPP
+
+#include "ServerConfig.hpp"
+
+class HttpConfig
+{
+	private:
+    	std::string root;
+    	std::string index;
+		bool autoindex;
+		size_t max_body_size;
+		std::map<int , std::string> error_pages;
+		std::vector<std::string> allowed_methods;
+    	std::vector<ServerConfig> servers;
+	
+	public:
+		HttpConfig();
+		HttpConfig(const HttpConfig& other);
+		HttpConfig&	operator=(const HttpConfig& other);
+		~HttpConfig();
+
+		const std::string&	getRoot() const;
+		const std::string&	getIndex() const;
+		const bool	getAutoindex() const;
+		const size_t getMaxBodySize() const;
+		const std::map<int, std::string>&	getErrorPages() const;
+		const std::vector<std::string>&	getMethods() const;
+		const std::vector<ServerConfig>& getServers() const;
+
+		void	setRoot(const std::string& root);
+		void	setIndex(const std::string& index);
+		void	setAutoindex(bool autoindex);
+		void	setMaxBodySize(size_t size);
+		void	addErrorPage(int code, const std::string& page);
+		void	addAllowedMethod(const std::string& method);
+		void	addServer(const ServerConfig& server);
+};
+
+#endif
---
+++ LocationConfig.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   LocationConfig.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maemran <maemran@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 11:19:51 by maemran           #+#    #+#             */
+/*   Updated: 2026/02/23 11:48:26 by maemran          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LOCATIONCONFIG_HPP
+#define LOCATIONCONFIG_HPP
+
+#include <string>
+#include <vector> 
+#include <map>
+
+class LocationConfig
+{
+	private:
+    	std::string path;
+    	std::string root;
+    	std::string index;
+		bool autoindex;
+		std::map<int , std::string> error_pages;
+		std::map<int , std::string> redirections;
+   		std::vector<std::string> allowed_methods;
+		size_t max_body_size;
+
+	public:
+		LocationConfig();
+		LocationConfig(const LocationConfig& other);
+		LocationConfig&	operator=(const LocationConfig& other);
+		~LocationConfig();
+
+		const std::string&	getPath() const;
+		const std::string&	getPath() const;
+		const std::string&	getIndex() const;
+		const bool	getAutoindex() const;
+		const std::vector<std::string>&	getMethods() const;
+		const std::map<int, std::string>&	getErrorPages() const;
+		const std::map<int, std::string>&	getRedirections() const;
+		const size_t getMaxBodySize() const;
+		
+		void	setPath(const std::string& path);
+		void	setRoot(const std::string& root);
+		void	setIndex(const std::string& index);
+		void	setAutoindex(bool autoindex);
+		void	addAllowedMethod(const std::string& method);
+		void	addErrorPage(int code, const std::string& page);
+		void	addRedirection(int code, const std::string& url);
+		void	setMaxBodySize(size_t size);
+};
+
+#endif
